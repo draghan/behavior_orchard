df5a8e54c5cba0c32a496f4005a4a9ab7ec9ad60
draghan <draghan2@wp.pl>
2020-04-08 00:01:45 +0200

    Implemented initial layout
    Initial layout was designed in the wxFormBuilder.
    I think menu as the ribbon will be handy for working with the editor.
    Maybe I'll reconsider the form of the properies panel but for now it's ok-ish.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Visual editor of behavior trees
 Based on wxWidgets 3. 
 
 Uses wxFormBuilder.
+
+Uses icons made by Gregor Cresnar from www.flaticon.com.
--- behavior_orchard/frames/Autogenerated.h
+++ behavior_orchard/frames/Autogenerated.h
@@ -10,11 +10,25 @@
 #include <wx/artprov.h>
 #include <wx/xrc/xmlres.h>
 #include <wx/string.h>
-#include <wx/frame.h>
+#include <wx/bitmap.h>
+#include <wx/image.h>
+#include <wx/icon.h>
 #include <wx/gdicmn.h>
 #include <wx/font.h>
 #include <wx/colour.h>
 #include <wx/settings.h>
+#include <wx/ribbon/buttonbar.h>
+#include <wx/ribbon/panel.h>
+#include <wx/ribbon/page.h>
+#include <wx/ribbon/control.h>
+#include <wx/ribbon/art.h>
+#include <wx/ribbon/bar.h>
+#include <wx/scrolwin.h>
+#include <wx/sizer.h>
+#include <wx/statbox.h>
+#include <wx/textctrl.h>
+#include <wx/stc/stc.h>
+#include <wx/frame.h>
 
 ///////////////////////////////////////////////////////////////////////////
 
@@ -26,6 +40,28 @@ class GeneratedMainFrame : public wxFrame
 	private:
 
 	protected:
+		wxRibbonBar* m_ribbonBar1;
+		wxRibbonPage* ribbon_page_general;
+		wxRibbonPanel* ribbon_panel_file;
+		wxRibbonButtonBar* m_ribbonButtonBar1;
+		wxRibbonPanel* ribbon_panel_misc;
+		wxRibbonButtonBar* m_ribbonButtonBar6;
+		wxRibbonPage* ribbon_page_nodes;
+		wxRibbonPanel* ribbon_panel_create;
+		wxRibbonButtonBar* m_ribbonButtonBar21;
+		wxRibbonPanel* ribbon_panel_edit;
+		wxRibbonButtonBar* m_ribbonButtonBar611;
+		wxRibbonPanel* ribbon_panel_goto;
+		wxRibbonButtonBar* m_ribbonButtonBar32;
+		wxRibbonPanel* ribbon_panel_show;
+		wxRibbonButtonBar* m_ribbonButtonBar311;
+		wxScrolledWindow* workspace;
+		wxTextCtrl* m_textCtrl1;
+		wxTextCtrl* m_textCtrl2;
+		wxTextCtrl* m_textCtrl3;
+		wxTextCtrl* m_textCtrl4;
+		wxTextCtrl* m_textCtrl5;
+		wxStyledTextCtrl* m_scintilla1;
 
 		// Virtual event handlers, overide them in your derived class
 		virtual void MainFrameOnClose( wxCloseEvent& event ) { event.Skip(); }
